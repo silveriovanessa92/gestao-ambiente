39708790700d18031c1b8119da0cd2493ac7baf8
Vanessa <0001156601@senaimgaluno.com.br>
2026-06-18 21:51:22 -0300

    ajusta banco railway

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_DATABASE || 'Gestão_Ambiente', 
-    port: process.env.DB_PORT
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: Number(process.env.DB_PORT)
 });
 
 pool.getConnection((err, connection) => {
@@ -14,8 +14,9 @@ pool.getConnection((err, connection) => {
         console.error('Erro ao conectar ao banco de dados:', err.message);
         return;
     }
+
     console.log('Conectado ao banco de dados MySQL!');
     connection.release();
 });
 
-module.exports = pool
+module.exports = pool;
